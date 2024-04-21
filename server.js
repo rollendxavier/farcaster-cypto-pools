99ef0797e77be199b50a2321ab028271c6d275cd
Rollend Xavier <rollendxavier@gmail.com>
2024-04-21 12:55:17 +0800

    fix-key

--- server.js
+++ server.js
@@ -8,6 +8,19 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get('/api/pools', async (req, res) => {
+  try {
+    const response = await axios.get('https://pro-api.coingecko.com/api/v3/onchain/search/pools', {
+      headers: { 'x-cg-pro-api-key': process.env.REACT_APP_COINGECKO_API_PRO_KEY },
+      params: { query: 'weth', network: 'eth', include: 'dex', page: 1 }
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Error fetching data' });
+  }
+});
+
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/build')));
 
